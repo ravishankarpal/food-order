f3e86772887ef268d04cc881838728a662d5da6e
ravishankarpal <55776769+ravishankarpal@users.noreply.github.com>
2021-11-28 19:48:13 +0530

    Update footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!--Footer Section Starts-->
 <div class="footer">
         <div class="wrapper">
-            <p class="text-center">&copy; All rights reserved. Designed By <a href="#">Abhinav Arya</a>, <a href="#"> Ravi Shankar Pal</a>, <a href="#">Ritika Mishra</a> and  <a href="#">Sonam Verma</a></p>
+            <p class="text-center">&copy; All rights reserved. Designed By  <a href="#"> Ravi Shankar Pal</a></p>
         </div>
     </div>
     <!--Footer Section Ends-->
